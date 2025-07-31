f06ec0f28462828d4a82f9db78e36429c88d81eb
coder-ph <phelixmbani@gmail.com>
2025-07-31 18:54:10 +0300

    add queryhistory models for storing query ist and llm resp

--- app/db/models.py
+++ app/db/models.py
@@ -5,4 +5,14 @@ import uuid
 from app.db.database import Base
 
 class QueryHistory(Base):
-    __table
+    __tablename__ = 'query_hist'
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default= uuid.uuid4, index=True)
+    user_id = Column(String, index=True, nullable=False)
+    query_id = Column(String, Unique=True, nullable=False)
+    query_text = Column(Text, nullable=False)
+    response_text = Column(Text, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    
+    def __repr__(self):
+        return f'[QueryHistory(user_id={self.user_id}, query_id={self.query_id}, timestamp={self.timestamp})]'
